5d1264bc966f1fedee15c2d8c3fb8f18e574b178
夏鹏程 <spectres@foxmail.com>
2021-07-27 17:17:54 +0800

    add e2e test sample

---
+++ test/tag.e2e-spec.ts
@@ -0,0 +1,44 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+import { TagFindAllResDto } from '../src/modules/tag/dtos/tag-find-all.res.dto';
+import { Model } from 'mongoose';
+import { Tag, TagDocument } from '../src/modules/tag/schemas/tag.schema';
+import { getModelToken, MongooseModule } from '@nestjs/mongoose';
+
+describe('hippo (e2e)', () => {
+  let app: INestApplication;
+  let tagModel: Model<TagDocument>;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [MongooseModule.forRoot('mongodb://localhost/e2e-test', { useCreateIndex: true }), AppModule]
+    }).compile();
+
+    tagModel = moduleFixture.get(getModelToken(Tag.name));
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('tags (GET)', () => {
+    beforeAll(async () => {
+      await tagModel.create({ tag: 'e2e1' });
+    });
+
+    afterAll(async () => {
+      await tagModel.deleteMany({});
+    });
+
+    it('获取 tag 列表', async () => {
+      const { body, status } = await request(app.getHttpServer()).get('/tags');
+      expect(status).toBe(200);
+      expect((body as TagFindAllResDto[]).length > 0).toBe(true);
+    });
+  });
+});
