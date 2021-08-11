de16109a5f2df79a8789b9fc6bbe8d979d54895a
夏鹏程 <spectres@foxmail.com>
2021-08-11 15:53:04 +0800

    add jwt expires_in response

--- src/modules/common/dtos/token.res.dto.ts
+++ src/modules/common/dtos/token.res.dto.ts
@@ -1,3 +1,4 @@
 export class TokenResDto {
   access_token: string;
+  jwt_expires_in: number;
 }
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,4 +1,5 @@
-import { BadRequestException, Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Delete, Get, Inject, Param, Post, Put, UseGuards } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthUser } from '../article/decorators/user.decorator';
 import { AuthService } from '../auth/auth.service';
@@ -6,6 +7,7 @@ import { IdReqDto } from '../common/dtos/id.req.dto';
 import { IdResDto } from '../common/dtos/id.res.dto';
 import { NumberResDto } from '../common/dtos/number.res.dto';
 import { TokenResDto } from '../common/dtos/token.res.dto';
+import { CommonConfigRegister } from '../config/registers/common.register';
 import { UserCreateUpdateReqDto } from './dtos/user-create-update.req.dto';
 import { UserGetResDto } from './dtos/user-get.res.dto';
 import { UserLoginReqDto } from './dtos/user-login.req.dto';
@@ -13,14 +15,19 @@ import { UserService } from './user.service';
 
 @Controller('users')
 export class UserController {
-  constructor(private readonly userService: UserService, private readonly authService: AuthService) {}
+  constructor(
+    @Inject(CommonConfigRegister.KEY)
+    private commonConfig: ConfigType<typeof CommonConfigRegister>,
+    private readonly userService: UserService,
+    private readonly authService: AuthService
+  ) {}
 
   @Post('/login')
   async login(@Body() body: UserLoginReqDto): Promise<TokenResDto> {
     const user = await this.authService.validateUser(body.email, body.password);
     if (!user) throw new BadRequestException('邮箱或密码不正确');
 
-    return { access_token: await this.authService.certificate(user) };
+    return { access_token: await this.authService.certificate(user), jwt_expires_in: this.commonConfig.jwtExpiresIn };
   }
 
   @UseGuards(AuthGuard('jwt'))
